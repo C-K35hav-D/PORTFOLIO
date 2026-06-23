9f22b5b0d1730369c6a784e086c730ce1e3bf5b5
C-K35hav-D <channakeshavdevaramani@gmail.com>
2026-06-23 22:20:50 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
 import Typewriter from "@/components/fancy/text/typewriter";
 import ImageTrail, { ImageTrailItem } from "@/components/fancy/image/image-trail";
-import render1 from './assets/trail-images/render1.webp';
-import render2 from './assets/trail-images/render2.webp';
-import render3 from './assets/trail-images/render3.webp';
-import render4 from './assets/trail-images/render4.webp';
-import render5 from './assets/trail-images/render5.webp';
-import render6 from './assets/trail-images/render6.webp';
+import render1 from './src/assets/trail-images/render1.webp';
+import render2 from './src/assets/trail-images/render2.webp';
+import render3 from './src/assets/trail-images/render3.webp';
+import render4 from './src/assets/trail-images/render4.webp';
+import render5 from './src/assets/trail-images/render5.webp';
+import render6 from './src/assets/trail-images/render6.webp';
 
 const trailItems = [
   { src: {render1},
