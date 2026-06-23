84581506723840b85600c639a4209e852516eb08
C-K35hav-D <channakeshavdevaramani@gmail.com>
2026-06-23 13:59:42 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,6 @@
 import { motion } from "framer-motion";
 import Typewriter from "@/components/fancy/text/typewriter";
 import ImageTrail, { ImageTrailItem } from "@/components/fancy/image/image-trail";
-import render1 from "../assets/trail-images/render1.webp";
-import render2 from "../assets/trail-images/render2.webp";
-import render3 from "../assets/trail-images/render3.webp";
-import render4 from "../assets/trail-images/render4.webp";
-import render5 from "../assets/trail-images/render5.webp";
-import render6 from "../assets/trail-images/Render6.webp";
 
 const trailItems = [
   { src: "https://raw.githubusercontent.com/C-K35hav-D/PORTFOLIO/refs/heads/main/src/assets/trail-images/render1.webp" },
