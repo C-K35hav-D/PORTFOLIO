3f5a2791488d7a2ff3c3e0d5e71d4d26a9b5d04f
C-K35hav-D <channakeshavdevaramani@gmail.com>
2026-06-24 19:30:13 +0000

    corrected path

--- src/components/Herobackup.jsx
+++ src/components/Herobackup.jsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
 import Typewriter from "@/components/fancy/text/typewriter";
 import ImageTrail, { ImageTrailItem } from "@/components/fancy/image/image-trail";
-import render1 from "/trail-images/render1.png";
-import render2 from "/trail-images/render2.png";
-import render3 from "/trail-images/render3.png";
-import render4 from "/trail-images/render4.png";
-import render5 from "/trail-images/render5.png";
-import render6 from "/trail-images/render6.png";
+import render1 from "./trail-images/render1.png";
+import render2 from "./trail-images/render2.png";
+import render3 from "./trail-images/render3.png";
+import render4 from "./trail-images/render4.png";
+import render5 from "./trail-images/render5.png";
+import render6 from "./trail-images/render6.png";
 
   
 const trailItems = [
